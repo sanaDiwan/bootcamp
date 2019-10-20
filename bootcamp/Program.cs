3138cabb942ca150d9b3793ab944c7a98db37405
sdiwan <sdiwan@eshopworld.com>
2019-10-20 19:37:47 +0100

    instantiating using dependency injection

---
+++ bootcamp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace bootcamp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //setup our DI
+            var serviceProvider = new ServiceCollection()
+                .AddSingleton<IMakeAnInstanceOfMe, MakeAnInstanceOfMe>()
+                .BuildServiceProvider();
+
+            //do the actual work here
+            var makeAnInstanceOfMe = serviceProvider.GetService<IMakeAnInstanceOfMe>();
+            makeAnInstanceOfMe.sayHello();
+        }
+    }
+
+    public class MakeAnInstanceOfMe: IMakeAnInstanceOfMe
+    {
+        public void sayHello()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public interface IMakeAnInstanceOfMe
+    {
+        void sayHello();
+    }
+}
